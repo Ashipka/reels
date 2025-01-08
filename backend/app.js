cfca0393c4f84aeed4eb2af62dec843e2c593657
oleksandr.shypka <a.s.shipka@gmail.com>
2025-01-09 00:46:11 +0100

    port update

--- backend/app.js
+++ backend/app.js
@@ -62,5 +62,5 @@ app.get("*", (req, res) => {
 });
 
 // Start the server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
