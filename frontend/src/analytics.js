62ef71232957fa6bd982350b45dd53025a09c17a
oleksandr.shypka <a.s.shipka@gmail.com>
2025-02-05 17:37:11 +0100

    Switching to react-ga4

--- frontend/src/analytics.js
+++ frontend/src/analytics.js
@@ -1,12 +1,16 @@
-import ReactGA from 'react-ga';
+// analytics.js
+import ReactGA from 'react-ga4';
 
-const TRACKING_ID = 'G-L5C8VZRY2W';
+const TRACKING_ID = 'G-L5C8VZRY2W'; // Your GA4 Measurement ID
 
 export const initGA = () => {
+  console.log("Initializing GA with ID:", TRACKING_ID);
   ReactGA.initialize(TRACKING_ID);
 };
 
 export const logPageView = () => {
-  ReactGA.set({ page: window.location.pathname });
-  ReactGA.pageview(window.location.pathname);
+  const pagePath = window.location.pathname + window.location.search;
+  console.log("Logging pageview for:", pagePath);
+  // With react-ga4, you send pageviews like this:
+  ReactGA.send({ hitType: "pageview", page: pagePath });
 };
